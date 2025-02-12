bfd1939e46b911271bee97c8b290073a67293929
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-02-12 14:36:40 -0800

    Add missing Android for react-native-screens

--- android/app/src/main/java/com/freightermobile/MainActivity.kt
+++ android/app/src/main/java/com/freightermobile/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.freightermobile
 
+// this import is needed for the onCreate override function
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +22,11 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * react-native-screens package requires this override to properly work on Android devices.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
