e6d0e66e360bd368bd7bba89f87d4e88bb7de62d
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-02-21 17:57:02 -0800

    Add JSDoc for getSrcDirs helper

--- config/getSrcDirs.js
+++ config/getSrcDirs.js
@@ -1,8 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 const fs = require("fs");
 const path = require("path");
 
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/**
+ * This function is used to get the source directories for the project.
+ * It is used to configure the babel and jest paths so that devs don't
+ * need to worry about manually configuring the paths when adding new
+ * directories.
+ *
+ * @param {string} rootDir - The root directory of the project.
+ * @param {string} format - The format of the output.
+ * @returns {Object} - The source directories.
+ */
+
 function getSrcDirs(rootDir, format = "babel") {
   const srcPath = path.resolve(rootDir, "src");
   const directories = fs
