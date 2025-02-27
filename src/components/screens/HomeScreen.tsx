6cecc2e2b6cd2efd96158e13bc49167d3a51ed7d
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-02-26 17:57:58 -0800

    Add support for SVG images

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -1,24 +1,33 @@
 import { BaseLayout } from "components/layout/BaseLayout";
-import { THEME } from "config/sds/theme";
-import { fs } from "helpers/dimensions";
+import { PALETTE, THEME } from "config/theme";
+import { fs, px, pxValue } from "helpers/dimensions";
 import React from "react";
 import styled from "styled-components/native";
+import { Button } from "components/sds/Button";
+
+import ClipboardIcon from "assets/icons/clipboard.svg";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  margin-horizontal: ${px(24)};
 `;
 
 const ScreenText = styled.Text`
   color: ${THEME.colors.text.primary};
   font-size: ${fs(16)};
+  margin-bottom: ${px(50)};
 `;
 
 export const HomeScreen = () => (
   <BaseLayout>
     <Container>
       <ScreenText>Home</ScreenText>
+
+      <Button secondary lg isFullWidth icon={<ClipboardIcon width={pxValue(16)} height={pxValue(16)} stroke={PALETTE.dark.gray["09"]} />}>
+        Test Button with Icon
+      </Button>
     </Container>
   </BaseLayout>
 );
