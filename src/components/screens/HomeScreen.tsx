4d78d23e64513bc7a9f239ff2de356934ded9a61
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-03-21 14:37:26 -0700

    Fetch balances from HomeScreen instead of BalancesList

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -1,7 +1,9 @@
 import { BalancesList } from "components/BalancesList";
 import { BaseLayout } from "components/layout/BaseLayout";
 import { Text } from "components/sds/Typography";
-import React from "react";
+import { NETWORKS } from "config/constants";
+import { useBalancesStore } from "ducks/balances";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -13,13 +15,30 @@ const Header = styled.View`
   margin-bottom: 20px;
 `;
 
-export const HomeScreen = () => (
-  <BaseLayout>
-    <Container>
-      <Header>
-        <Text md>Tokens</Text>
-      </Header>
-      <BalancesList />
-    </Container>
-  </BaseLayout>
-);
+export const HomeScreen = () => {
+  // TODO: Get this from wallet context
+  const publicKey = "GAZAJVMMEWVIQRP6RXQYTVAITE7SC2CBHALQTVW2N4DYBYPWZUH5VJGG";
+  const network = NETWORKS.TESTNET;
+
+  const fetchAccountBalances = useBalancesStore(
+    (state) => state.fetchAccountBalances,
+  );
+
+  useEffect(() => {
+    fetchAccountBalances({
+      publicKey,
+      network,
+    });
+  }, [fetchAccountBalances, publicKey, network]);
+
+  return (
+    <BaseLayout>
+      <Container>
+        <Header>
+          <Text md>Tokens</Text>
+        </Header>
+        <BalancesList publicKey={publicKey} network={network} />
+      </Container>
+    </BaseLayout>
+  );
+};
