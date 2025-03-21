0876e5ad0391bd9350823b152e8d8c359b4115db
Bruno Nascimento <brunonascimentodev@gmail.com>
2025-03-21 19:26:43 -0300

    chore: move shim import to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+/* eslint-disable @fnando/consistent-import/consistent-import */
+// Don't remove the shim import. It's necessary for the app to work. It should be the first import.
+import "./shim";
 import "./src/index";
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// The order is important here. shim must be imported first.
 /* eslint-disable @fnando/consistent-import/consistent-import */
 import { App } from "components/App";
 import { AppRegistry } from "react-native";
 
 import { name as appName } from "../app.json";
-import "../shim";
 
 AppRegistry.registerComponent(appName, () => App);
