f51531808189ae27c709ad532952f6c3bc020460
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-03-29 11:05:20 -0700

    Add Account title and buttons to home screen

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import { BalancesList } from "components/BalancesList";
+import { IconButton } from "components/IconButton";
 import { BaseLayout } from "components/layout/BaseLayout";
 import Avatar from "components/sds/Avatar";
+import Icon from "components/sds/Icon";
 import { Display, Text } from "components/sds/Typography";
 import { TEST_NETWORK_DETAILS, TEST_PUBLIC_KEY } from "config/constants";
 import { THEME } from "config/theme";
 import { px } from "helpers/dimensions";
+import useAppTranslation from "hooks/useAppTranslation";
 import React from "react";
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
@@ -12,9 +15,8 @@ import styled from "styled-components/native";
 const { width } = Dimensions.get("window");
 
 const TopSection = styled.View`
-  margin-top: ${px(86)};
-  padding-top: ${px(48)};
-  gap: ${px(32)};
+  margin-top: ${px(50)};
+  padding-top: ${px(22)};
   width: 100%;
   align-items: center;
 `;
@@ -31,6 +33,14 @@ const AccountNameRow = styled.View`
   align-items: center;
 `;
 
+const ButtonsRow = styled.View`
+  flex-direction: row;
+  gap: ${px(24)};
+  align-items: center;
+  justify-content: center;
+  margin-vertical: ${px(32)};
+`;
+
 const BorderLine = styled.View`
   width: ${width}px;
   margin-left: ${px(-24)};
@@ -42,6 +52,8 @@ const BorderLine = styled.View`
 export const HomeScreen = () => {
   const publicKey = TEST_PUBLIC_KEY;
   const networkDetails = TEST_NETWORK_DETAILS;
+
+  const { t } = useAppTranslation();
 
   return (
     <BaseLayout>
@@ -55,6 +67,21 @@ export const HomeScreen = () => {
             $1,305.13
           </Display>
         </AccountTotal>
+
+        <ButtonsRow>
+          <IconButton Icon={Icon.Plus} title={t("home.buy")} />
+          <IconButton
+            Icon={Icon.ArrowUp}
+            title={t("home.send")}
+            onPress={() => {}}
+          />
+          <IconButton Icon={Icon.RefreshCw02} title={t("home.swap")} />
+          <IconButton
+            Icon={Icon.Copy01}
+            title={t("home.copy")}
+            onPress={() => {}}
+          />
+        </ButtonsRow>
       </TopSection>
       <BorderLine />
       <BalancesList publicKey={publicKey} network={networkDetails.network} />
