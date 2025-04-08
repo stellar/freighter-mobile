6c98aec53a5237e44b1a5026585263f3dbde3fb7
Bruno Nascimento <brunonascimentodev@gmail.com>
2025-04-08 10:37:00 -0300

    feat(wip): add ManageAssetsScreen

--- src/components/screens/ManageAssetsScreen.tsx
+++ src/components/screens/ManageAssetsScreen.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unstable-nested-components */
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { BalancesList } from "components/BalancesList";
 import { BaseLayout } from "components/layout/BaseLayout";
 import Icon from "components/sds/Icon";
 import { Input } from "components/sds/Input";
@@ -8,19 +9,41 @@ import {
   ManageAssetsStackParamList,
 } from "config/routes";
 import { THEME } from "config/theme";
-import { pxValue } from "helpers/dimensions";
+import { useAuthenticationStore } from "ducks/auth";
+import { px, pxValue } from "helpers/dimensions";
 import useAppTranslation from "hooks/useAppTranslation";
+import useGetActiveAccount from "hooks/useGetActiveAccount";
 import React, { useEffect, useState } from "react";
 import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
 type ManageAssetsScreenProps = NativeStackScreenProps<
   ManageAssetsStackParamList,
   typeof MANAGE_ASSETS_ROUTES.MANAGE_ASSETS_SCREEN
 >;
 
+const Spacer = styled.View`
+  height: ${px(16)};
+`;
+
+// const icons = Platform.select({
+//   ios: {
+//     copyAddress: "doc.on.doc",
+//     hideAsset: "eye.slash",
+//     removeAsset: "minus.circle",
+//   },
+//   android: {
+//     copyAddress: "baseline_format_paint",
+//     hideAsset: "baseline_delete",
+//     removeAsset: "outline_circle",
+//   },
+// });
+
 const ManageAssetsScreen: React.FC<ManageAssetsScreenProps> = ({
   navigation,
 }) => {
+  const { account } = useGetActiveAccount();
+  const { network } = useAuthenticationStore();
   const [search, setSearch] = useState("");
   const { t } = useAppTranslation();
 
@@ -42,10 +65,58 @@ const ManageAssetsScreen: React.FC<ManageAssetsScreenProps> = ({
     });
   }, [navigation, t]);
 
+  // const actionsOnPress = {
+  //   [t("manageAssetsScreen.actions.copyAddress")]: (balance: PricedBalance) =>
+  //     Clipboard.setString(balance.tokenCode ?? ""),
+  //   [t("manageAssetsScreen.actions.hideAsset")]: () =>
+  //     logger.debug("ManageAssetsScreen", "hideAsset Not implemented"),
+  //   [t("manageAssetsScreen.actions.removeAsset")]: () =>
+  //     logger.debug("ManageAssetsScreen", "removeAsset Not implemented"),
+  // };
+
+  // const actions = [
+  //   {
+  //     inlineChildren: true,
+  //     disabled: true,
+  //     actions: [
+  //       {
+  //         title: t("manageAssetsScreen.actions.copyAddress"),
+  //         systemIcon: icons!.copyAddress,
+  //       },
+  //       {
+  //         title: t("manageAssetsScreen.actions.hideAsset"),
+  //         systemIcon: icons!.hideAsset,
+  //       },
+  //     ],
+  //     title: "",
+  //   },
+  //   {
+  //     title: t("manageAssetsScreen.actions.removeAsset"),
+  //     systemIcon: icons!.removeAsset,
+  //     destructive: true,
+  //   },
+  // ];
+
+  // const rightContent = (balance: PricedBalance) => (
+  //   <ContextMenuButton
+  //     contextMenuProps={{
+  //       onPress: (e) => {
+  //         actionsOnPress[e.nativeEvent.name](balance);
+  //       },
+  //       actions,
+  //     }}
+  //   >
+  //     <Icon.DotsHorizontal
+  //       size={pxValue(24)}
+  //       color={THEME.colors.foreground.primary}
+  //     />
+  //   </ContextMenuButton>
+  // );
+
   return (
     <BaseLayout
-      useKeyboardAvoidingView
       insets={{ bottom: false, left: true, right: true, top: false }}
+      useKeyboardAvoidingView
     >
       <Input
         placeholder={t("manageAssetsScreen.searchPlaceholder")}
@@ -59,8 +130,9 @@ const ManageAssetsScreen: React.FC<ManageAssetsScreenProps> = ({
           />
         }
       />
+      <Spacer />
+      <BalancesList publicKey={account?.publicKey ?? ""} network={network} />
     </BaseLayout>
   );
 };
-
 export default ManageAssetsScreen;
