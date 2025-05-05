b5db3b0cbf2955e193a990fed6028f674af2690d
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-05-05 17:09:15 -0300

    Install Reown packages

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @fnando/consistent-import/consistent-import */
+// NOTE: please keep this react-native-compat import at the top of the file
+import "@walletconnect/react-native-compat";
 import { App } from "components/App";
 import { AppRegistry } from "react-native";
 
