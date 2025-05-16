852fa85bcd1b361f86c26d9aae32cf126d01308c
Danilo Silva <danilo.silva@ckl.io>
2025-05-16 14:20:20 -0300

    feat: added some settings screen urls

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -24,7 +24,12 @@ export const VISUAL_DELAY_MS = 500;
 export const DEFAULT_DECIMALS = 7;
 export const FIAT_DECIMALS = 2;
 
+// settings screen URLs
 export const FREIGHTER_FAQ_URL = "https://www.freighter.app/faq";
+export const FREIGHTER_DISCORD_URL = "https://discord.gg/AkakAShY"; // TODO: switch to the permalink
+export const FREIGHTER_GITHUB_ISSUE_URL =
+  "https://github.com/stellar/freighter-mobile/issues";
+
 export const FREIGHTER_BACKEND_URL =
   "https://freighter-backend-prd.stellar.org/api/v1";
 export const CREATE_ACCOUNT_URL =
