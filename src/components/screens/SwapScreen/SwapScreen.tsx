619f80ad23e398e4ee1afa6a9f39dfff8594679e
Danilo Silva <danilo.silva@ckl.io>
2025-06-12 15:05:30 -0300

    feat: added swap from screen

--- src/components/screens/SwapScreen/SwapScreen.tsx
+++ src/components/screens/SwapScreen/SwapScreen.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { BalancesList } from "components/BalancesList";
+import { BaseLayout } from "components/layout/BaseLayout";
+import Icon from "components/sds/Icon";
+import { Input } from "components/sds/Input";
 import { SWAP_ROUTES, SwapStackParamList } from "config/routes";
-import React from "react";
-import { View } from "react-native";
+import { useAuthenticationStore } from "ducks/auth";
+import useAppTranslation from "hooks/useAppTranslation";
+import useColors from "hooks/useColors";
+import useGetActiveAccount from "hooks/useGetActiveAccount";
+import React, { useEffect, useState } from "react";
+import { TouchableOpacity, View } from "react-native";
 
 type SwapScreenProps = NativeStackScreenProps<
   SwapStackParamList,
@@ -9,7 +18,67 @@ type SwapScreenProps = NativeStackScreenProps<
 >;
 
 const SwapScreen: React.FC<SwapScreenProps> = ({ navigation }) => {
-  return <View></View>;
+  const { account } = useGetActiveAccount();
+  const {
+    network,
+  } = useAuthenticationStore();
+  const { themeColors } = useColors();
+  const { t } = useAppTranslation();
+  const [searchToken, setSearchToken] = useState("");
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon.X size={24} color={themeColors.base[1]} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation,  themeColors]);
+
+  const handleTokenPress = (tokenId: string) => {
+    console.log(tokenId);
+  };
+
+  const handleSearch = (text: string) => {
+    console.log(text);
+  };
+
+  const handlePasteFromClipboard = () => {
+    console.log("paste from clipboard");
+  };
+
+  return (
+    <BaseLayout insets={{ top: false }}>
+      <View className="flex-1">
+        <View className="mb-8 mt-4">
+          <Input
+            fieldSize="md"
+            leftElement={
+              <Icon.SearchMd
+                size={16}
+                color={themeColors.foreground.primary}
+              />
+            }
+            testID="search-input"
+            placeholder={t("swapScreen.searchTokenInputPlaceholder")}
+            onChangeText={handleSearch}
+            endButton={{
+              content: t("common.paste"),
+              onPress: handlePasteFromClipboard,
+            }}
+            value={searchToken}
+          />
+        </View>
+        <BalancesList
+          publicKey={account?.publicKey ?? ""}
+          network={network}
+          onTokenPress={handleTokenPress}
+          showTitleIcon
+        />
+      </View>
+    </BaseLayout>
+  );
 };
 
 export default SwapScreen;
