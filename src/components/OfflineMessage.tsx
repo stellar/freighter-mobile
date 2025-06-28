e2d4b17a528f0f8999abe5b2731fa5b7bdcf61b9
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-06-27 17:49:06 -0700

    Tweak padding

--- src/components/OfflineMessage.tsx
+++ src/components/OfflineMessage.tsx
@@ -13,7 +13,7 @@ export const OfflineMessage = () => {
       className="absolute top-0 left-0 right-0 bg-status-error z-10"
       style={{ paddingTop: insets.top }}
     >
-      <View className="p-3 items-center">
+      <View className="p-1 items-center">
         <Text primary sm semiBold>
           {t("noInternetConnection")}
         </Text>
