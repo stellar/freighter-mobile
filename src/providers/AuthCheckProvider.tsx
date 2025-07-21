0361e029a8e2ed23d357911bacf76c0efc58fc8f
Danilo Silva <danilo.silva@ckl.io>
2025-07-21 18:23:09 -0300

    fix: moved identifyUser to RootNavigator

--- src/providers/AuthCheckProvider.tsx
+++ src/providers/AuthCheckProvider.tsx
@@ -1,8 +1,6 @@
-import { useAuthenticationStore } from "ducks/auth";
 import useAuthCheck from "hooks/useAuthCheck";
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode } from "react";
 import { View } from "react-native";
-import { analytics } from "services/analytics";
 
 interface AuthCheckProviderProps {
   children: ReactNode;
@@ -16,13 +14,6 @@ export const AuthCheckProvider: React.FC<AuthCheckProviderProps> = ({
   children,
 }) => {
   const { panHandlers } = useAuthCheck();
-  const { account } = useAuthenticationStore();
-
-  useEffect(() => {
-    if (account) {
-      analytics.identifyUser();
-    }
-  }, [account]);
 
   // The View with panHandlers will detect user interaction across the entire app
   return (
