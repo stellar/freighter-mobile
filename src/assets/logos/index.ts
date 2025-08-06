7801f5b22b6c5b50cfc756ad63605480031761ae
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2025-08-06 14:47:31 -0700

    Freighter logo 2d for QR Code

--- src/assets/logos/index.ts
+++ src/assets/logos/index.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @fnando/consistent-import/consistent-import */
 // Export all logo assets from this file for easy importing
 import blockaidLogo from "./blockaid-logo.svg";
-import freighterLogo from "./freighter-logo-2d.png";
+import freighterLogo2d from "./freighter-logo-2d.png";
 import stellarLogo from "./stellar-logo.png";
 import walletConnectLogo from "./walletconnect-logo.png";
 
 export const logos = {
   stellar: stellarLogo,
-  freighter: freighterLogo,
+  freighter2d: freighterLogo2d,
   blockaid: blockaidLogo,
   walletConnect: walletConnectLogo,
 };
