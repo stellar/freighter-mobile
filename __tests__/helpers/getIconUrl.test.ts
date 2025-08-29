1fd1ff95435b080b362c03522f92eb72517b54df
Aristides Staffieri <aristides.staffieri@stellar.org>
2025-08-29 14:35:19 -0600

    uncomments test, leftover from debug

--- __tests__/helpers/getIconUrl.test.ts
+++ __tests__/helpers/getIconUrl.test.ts
@@ -1,5 +1,6 @@
 import { NETWORKS } from "config/constants";
 import { getIconUrl } from "helpers/getIconUrl";
+import * as issuerModule from "helpers/getIconUrlFromIssuer";
 import * as tokenListModule from "helpers/getIconUrlFromTokensLists";
 
 describe("getIconUrl", () => {
@@ -26,45 +27,43 @@ describe("getIconUrl", () => {
     expect(tokenListModule.getIconUrlFromTokensLists).toHaveBeenCalledTimes(1);
   });
 
-  // it("falls back to issuer TOML if token lists returns undefined", async () => {
-  //   jest
-  //     .spyOn(tokenListModule, "getIconUrlFromTokensLists")
-  //     .mockResolvedValue(undefined);
-  //   jest
-  //     .spyOn(issuerModule, "getIconUrlFromIssuer")
-  //     .mockResolvedValue("https://issuer-icon.png");
+  it("falls back to issuer TOML if token lists returns undefined", async () => {
+    jest
+      .spyOn(tokenListModule, "getIconUrlFromTokensLists")
+      .mockResolvedValue(undefined);
+    jest
+      .spyOn(issuerModule, "getIconUrlFromIssuer")
+      .mockResolvedValue("https://issuer-icon.png");
 
-  //   const result = await getIconUrl({ asset, network, networkUrl });
+    const result = await getIconUrl({ asset, network });
 
-  //   expect(result).toBe("https://issuer-icon.png");
-  //   expect(tokenListModule.getIconUrlFromTokensLists).toHaveBeenCalledTimes(1);
-  //   expect(issuerModule.getIconUrlFromIssuer).toHaveBeenCalledTimes(1);
-  // });
+    expect(result).toBe("https://issuer-icon.png");
+    expect(tokenListModule.getIconUrlFromTokensLists).toHaveBeenCalledTimes(1);
+    expect(issuerModule.getIconUrlFromIssuer).toHaveBeenCalledTimes(1);
+  });
 
-  // it("returns undefined if both token lists and issuer TOML fail", async () => {
-  //   jest
-  //     .spyOn(tokenListModule, "getIconUrlFromTokensLists")
-  //     .mockResolvedValue(undefined);
-  //   jest
-  //     .spyOn(issuerModule, "getIconUrlFromIssuer")
-  //     .mockResolvedValue("");
+  it("returns undefined if both token lists and issuer TOML fail", async () => {
+    jest
+      .spyOn(tokenListModule, "getIconUrlFromTokensLists")
+      .mockResolvedValue(undefined);
+    jest.spyOn(issuerModule, "getIconUrlFromIssuer").mockResolvedValue("");
 
-  //   const result = await getIconUrl({ asset, network, networkUrl });
+    const result = await getIconUrl({ asset, network });
 
-  //   expect(result).toEqual("");
-  //   expect(tokenListModule.getIconUrlFromTokensLists).toHaveBeenCalledTimes(1);
-  //   expect(issuerModule.getIconUrlFromIssuer).toHaveBeenCalledTimes(1);
-  // });
+    expect(result).toEqual("");
+    expect(tokenListModule.getIconUrlFromTokensLists).toHaveBeenCalledTimes(1);
+    expect(issuerModule.getIconUrlFromIssuer).toHaveBeenCalledTimes(1);
+  });
 
-  // it("does not call issuer TOML if asset.issuer or asset.code is missing", async () => {
-  //   const assetMissing = { contractId: "C123" }; // no issuer/code
-  //   jest
-  //     .spyOn(tokenListModule, "getIconUrlFromTokensLists")
-  //     .mockResolvedValue(undefined);
+  it("does not call issuer TOML if asset.issuer or asset.code is missing", async () => {
+    const assetMissing = { contractId: "C123" }; // no issuer/code
+    jest
+      .spyOn(tokenListModule, "getIconUrlFromTokensLists")
+      .mockResolvedValue(undefined);
 
-  //   const result = await getIconUrl({ asset: assetMissing, network, networkUrl });
+    const result = await getIconUrl({ asset: assetMissing, network });
 
-  //   expect(result).toEqual("");
-  //   expect(issuerModule.getIconUrlFromIssuer).not.toHaveBeenCalled();
-  // });
+    expect(result).toEqual("");
+    expect(issuerModule.getIconUrlFromIssuer).not.toHaveBeenCalled();
+  });
 });
