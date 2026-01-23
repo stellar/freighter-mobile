e62084d9beadb7910469212f1ef1521b14ed0021
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2026-01-23 14:22:25 -0800

    Skip jailbroken check in CI env devices

--- src/helpers/deviceSecurity.ts
+++ src/helpers/deviceSecurity.ts
@@ -1,9 +1,16 @@
 import { AnalyticsEvent } from "config/analyticsConfig";
 import { logger } from "config/logger";
+import { isE2ETest } from "helpers/isEnv";
 import JailMonkey from "jail-monkey";
 import { analytics } from "services/analytics";
 
 export const isDeviceJailbroken = (): boolean => {
+  // Skip jail-monkey check for e2e tests as
+  // isJailBroken() returns "true" for the CI devices
+  if (isE2ETest) {
+    return false;
+  }
+
   try {
     const isJailBroken = JailMonkey.isJailBroken();
     if (isJailBroken) {
