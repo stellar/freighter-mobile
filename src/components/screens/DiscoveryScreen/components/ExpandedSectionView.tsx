d86ef45c4d529a1b0d69a3828dc768aecbb98eed
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2026-03-11 17:21:23 -0700

    Use existing CustomHeaderButton component

--- src/components/screens/DiscoveryScreen/components/ExpandedSectionView.tsx
+++ src/components/screens/DiscoveryScreen/components/ExpandedSectionView.tsx
@@ -1,12 +1,14 @@
+import {
+  CustomHeaderButton,
+  DEFAULT_HEADER_BUTTON_SIZE,
+} from "components/layout/CustomHeaderButton";
 import ProtocolRow from "components/screens/DiscoveryScreen/components/ProtocolRow";
 import { VerticalListItem } from "components/screens/DiscoveryScreen/components/VerticalListSection";
-import Icon from "components/sds/Icon";
 import { Text } from "components/sds/Typography";
-import { DEFAULT_PADDING, DEFAULT_PRESS_DELAY } from "config/constants";
+import { DEFAULT_PADDING } from "config/constants";
 import { pxValue } from "helpers/dimensions";
-import useColors from "hooks/useColors";
 import React, { useCallback, useMemo } from "react";
-import { View, FlatList, TouchableOpacity } from "react-native";
+import { View, FlatList } from "react-native";
 
 interface ExpandedSectionViewProps {
   title: string;
@@ -17,16 +19,8 @@ interface ExpandedSectionViewProps {
   onScrollEnd?: () => void;
 }
 
-const HEADER_BUTTON_SIZE = 24;
-
 const ExpandedSectionView: React.FC<ExpandedSectionViewProps> = React.memo(
   ({ title, items, onBack, onItemOpen, onItemPress, onScrollEnd }) => {
-    const { themeColors } = useColors();
-
-    const handleBack = useCallback(() => {
-      onBack();
-    }, [onBack]);
-
     const renderItem = useCallback(
       // eslint-disable-next-line react/no-unused-prop-types
       ({ item }: { item: VerticalListItem }) => (
@@ -60,20 +54,11 @@ const ExpandedSectionView: React.FC<ExpandedSectionViewProps> = React.memo(
     return (
       <View className="flex-1 bg-background-primary">
         <View className="flex-row items-center justify-between px-6 pb-4 pt-4">
-          <TouchableOpacity
-            onPress={handleBack}
-            hitSlop={{ top: 8, bottom: 8, left: 8, right: 8 }}
-            delayPressIn={DEFAULT_PRESS_DELAY}
-          >
-            <Icon.ArrowLeft
-              size={HEADER_BUTTON_SIZE}
-              color={themeColors.base[1]}
-            />
-          </TouchableOpacity>
+          <CustomHeaderButton position="left" onPress={onBack} />
           <Text md semiBold>
             {title}
           </Text>
-          <View style={{ width: HEADER_BUTTON_SIZE }} />
+          <View className={DEFAULT_HEADER_BUTTON_SIZE} />
         </View>
 
         <FlatList
