216fde0c9d6f940d113326e39196f3c1bb5ef5e4
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2026-03-19 22:22:10 -0700

    Remove placeholder image

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,7 @@
 /* eslint-disable @fnando/consistent-import/consistent-import */
 // Export all image assets from this file for easy importing
-import featuredBackgroundPlaceholderImage from "./featured-background-placeholder.png";
 import iPhoneFrameImage from "./iphone-frame.png";
 
 export const images = {
   iPhoneFrame: iPhoneFrameImage,
-  featuredBackgroundPlaceholder: featuredBackgroundPlaceholderImage,
 };
